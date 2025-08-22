3199f1dcd591ec8e477899e807e0c4cf4fcba1d3
SNEHA CHAUDHARY <snehachaudharyrajmal@gmail.com>
2025-08-22 14:17:20 +0530

    Time: 13 ms (24.44%), Space: 12.6 MB (14.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,8 +1,26 @@
-class Solution:
-    def twoSum(self, nums, target):
-        hashmap = {}
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hashmap:
-                return [hashmap[diff], i]
-            hashmap[num] = i
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            total = v1 + v2 + carry
+            carry = total // 10
+            curr.next = ListNode(total % 10)
+
+            curr = curr.next
+            if l1: 
+                l1 = l1.next
+            if l2: 
+                l2 = l2.next
+
+        return dummy.next
