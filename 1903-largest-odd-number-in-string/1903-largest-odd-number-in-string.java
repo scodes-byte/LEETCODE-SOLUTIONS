647939fcd0c885277b50b7d86fd7ba78ea511d29
SNEHA CHAUDHARY <snehachaudharyrajmal@gmail.com>
2026-06-18 10:07:25 +0530

    Time: 1 ms (99.81%), Space: 47.2 MB (34.84%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -1,19 +1,32 @@
+//class Solution {
+//    public String largestOddNumber(String num) {
+//        int i;
+//
+//        for (i = num.length() - 1; i >= 0; i--) {
+//            char ch = num.charAt(i);
+//
+//            if (ch == '1' || ch == '3' || ch == '5' || ch == '7' || ch == '9') {
+//                break;
+//            }
+//        }
+//
+//        if (i == -1) {
+//            return "";
+//        }
+//
+//        return num.substring(0, i + 1);
+//    }
+//}
+
 class Solution {
     public String largestOddNumber(String num) {
-        int i;
+        int n = num.length();
 
-        for (i = num.length() - 1; i >= 0; i--) {
-            char ch = num.charAt(i);
-
-            if (ch == '1' || ch == '3' || ch == '5' || ch == '7' || ch == '9') {
-                break;
+        for(int i = n-1; i >= 0; i--){
+            if(num.charAt(i) % 2 != 0){
+                return num.substring(0, i+1);
             }
         }
-
-        if (i == -1) {
-            return "";
-        }
-
-        return num.substring(0, i + 1);
+        return "";
     }
 }
