64b2e0d4ff97311ed74330e0016c33a117655ccb
SNEHA CHAUDHARY <snehachaudharyrajmal@gmail.com>
2026-07-01 00:02:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (97.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public void sortColors(int[] nums) {
        // Arrays.sort(nums);
         int n = nums.length;
@@ -11,6 +11,30 @@ class Solution {
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
                 }
+            }
+        }
+    }
+}*/
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
     }
