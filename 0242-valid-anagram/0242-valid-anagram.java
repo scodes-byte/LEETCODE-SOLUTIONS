81122cc8c70509f1d7b3ea5ab03fc456f8209106
SNEHA CHAUDHARY <snehachaudharyrajmal@gmail.com>
2026-07-01 10:08:57 +0530

    Time: 5 ms (61.83%), Space: 44.1 MB (94.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,33 +1,20 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) {
+        if (s.length() != t.length())
             return false;
-        }
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
-
-            if (!map.containsKey(ch)) {
+        for (int check : arr) {
+            if (check != 0)
                 return false;
-            }
-
-            map.put(ch, map.get(ch) - 1);
-
-            if (map.get(ch) == 0) {
-                map.remove(ch);
-            }
         }
 
-        return map.isEmpty();
+        return true;
     }
 }
