e086aab20878f48f8617b2ca159034d730aabb00
SNEHA CHAUDHARY <snehachaudharyrajmal@gmail.com>
2026-07-03 13:54:28 +0530

    Time: 4 ms (13.18%), Space: 43.1 MB (67.53%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,4 +1,4 @@
-public class Solution {
+/*public class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
         String prefix = strs[0];
@@ -6,5 +6,27 @@ public class Solution {
             while (s.indexOf(prefix) != 0)
                 prefix = prefix.substring(0, prefix.length() - 1);
         return prefix;
+    }
+}*/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+        String ans = "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+
+            char ch = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+
+                if (i == strs[j].length() || strs[j].charAt(i) != ch) {
+                    return ans;
+                }
+            }
+
+            ans += ch;
+        }
+
+        return ans;
     }
 }
