884a457a73539cf489b4aa2b95de6ef5bbe1111a
SNEHA CHAUDHARY <snehachaudharyrajmal@gmail.com>
2026-07-15 09:47:15 +0530

    Time: 35 ms (44.57%), Space: 58.9 MB (85.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,15 +1,37 @@
+/*class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        HashSet<List<Integer>> set = new HashSet<>();
+        int n =nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k =j+1;k<n;k++){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        set.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    }
+                }
+            }
+        }return new ArrayList(set) ;
+        
+    }
+}*/
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         List<List<Integer>> ans = new ArrayList<>();
+
         Arrays.sort(nums);
 
         int n = nums.length;
 
         for (int i = 0; i < n - 2; i++) {
 
-            if (i > 0 && nums[i] == nums[i - 1])
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
 
             int left = i + 1;
             int right = n - 1;
@@ -19,20 +41,33 @@ class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
 
                 if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    while (left < right && nums[left] == nums[left + 1])
-                        left++;
-
-                    while (left < right && nums[right] == nums[right - 1])
-                        right--;
+                    ans.add(Arrays.asList(
+                            nums[i],
+                            nums[left],
+                            nums[right]
+                    ));
 
                     left++;
                     right--;
 
-                } else if (sum < 0) {
+                    // Skip duplicates
+                    while (left < right &&
+                           nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right &&
+                           nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+
+                else if (sum < 0) {
                     left++;
-                } else {
+                }
+
+                else {
                     right--;
                 }
             }
